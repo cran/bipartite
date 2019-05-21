65bf12862ec093888256c9f135b573357eb9221a
Carsten F. Dormann <carsten.dormann@biom.uni-freiburg.de>
2019-05-21 16:20:04 +0000

    version 2.13

---
+++ inst/doc/Intro2bipartite.R
@@ -0,0 +1,185 @@
+### R code from vignette source 'Intro2bipartite.Rnw'
+### Encoding: UTF-8
+
+###################################################
+### code chunk number 1: setup
+###################################################
+library(knitr)
+opts_chunk$set(fig.path='figure/twocolumn-', fig.align='center', fig.show='hold', cache=TRUE)
+render_listings()
+
+
+###################################################
+### code chunk number 2: Intro2bipartite.Rnw:149-150
+###################################################
+library(bipartite)
+
+
+###################################################
+### code chunk number 3: Intro2bipartite.Rnw:158-161 (eval = FALSE)
+###################################################
+## par(xpd=T)
+## plotweb(motten1982)
+## visweb(motten1982)
+
+
+###################################################
+### code chunk number 4: Intro2bipartite.Rnw:185-186 (eval = FALSE)
+###################################################
+## plotPAC(PAC(motten1982), outby=0.9)
+
+
+###################################################
+### code chunk number 5: Intro2bipartite.Rnw:201-203 (eval = FALSE)
+###################################################
+## mod <- computeModules(motten1982)
+## plotModuleWeb(mod)
+
+
+###################################################
+### code chunk number 6: Intro2bipartite.Rnw:216-223 (eval = FALSE)
+###################################################
+## par(mfrow=c(1,2), xpd=T)
+## gplot(as.one.mode(motten1982, project="higher"), 
+##  label=colnames(motten1982), gmode="graph", 
+## label.cex=0.6, vertex.cex=2)
+## gplot(as.one.mode(motten1982, project="lower"), 
+## 	label=rownames(motten1982), gmode="graph", 
+## 	label.cex=0.6, vertex.cex=2, vertex.col="green")
+
+
+###################################################
+### code chunk number 7: Intro2bipartite.Rnw:286-288
+###################################################
+networklevel(bezerra2009, index=c("ISA", "weighted NODF", "Fisher alpha"), 
+             SAmethod="log")
+
+
+###################################################
+### code chunk number 8: Intro2bipartite.Rnw:318-320
+###################################################
+grouplevel(bezerra2009, level="both", index=c("mean number of links", "weighted 
+     cluster coefficient", "effective partners", "niche overlap"), dist="bray")
+
+
+###################################################
+### code chunk number 9: Intro2bipartite.Rnw:333-334
+###################################################
+str(linklevel(bezerra2009, index=c("dependence", "endpoint")))
+
+
+###################################################
+### code chunk number 10: Intro2bipartite.Rnw:365-367
+###################################################
+specieslevel(bezerra2009, level="lower", index=c("normalised degree", "PDI", 
+      "effective partners"), PDI.normalise=F)
+
+
+###################################################
+### code chunk number 11: networkPCA (eval = FALSE)
+###################################################
+## web.names <- data(package="bipartite")$results[,3]
+## data(list=web.names) #loads all webs
+## # the next step takes around 10 minutes:
+## netw.indic.webs <- t(sapply(web.names, function(x) networklevel(get(x), 
+##               index="ALLBUTDD")))
+
+
+###################################################
+### code chunk number 12: load external data
+###################################################
+#load("/Volumes/Macintosh HD/Users/Carsten/Data/aktuell/bipartite/bipartite/vignettes/figures/netw.indic.webs.Rdata")
+load("./figures/netw.indic.webs.Rdata") # loads the files stored above; avoids re-running this time-consuming analysis!
+
+
+###################################################
+### code chunk number 13: Intro2bipartite.Rnw:418-420 (eval = FALSE)
+###################################################
+## PCA.out <- prcomp(netw.indic.webs[,-5], scale.=T)
+## biplot(PCA.out, xpd=T, las=1)
+
+
+###################################################
+### code chunk number 14: Intro2bipartite.Rnw:426-427
+###################################################
+summary(PCA.out)
+
+
+###################################################
+### code chunk number 15: Intro2bipartite.Rnw:431-432
+###################################################
+PCA.out <- prcomp(netw.indic.webs[,-5], scale.=T)
+
+
+###################################################
+### code chunk number 16: Intro2bipartite.Rnw:434-435
+###################################################
+round(PCA.out$rotation[, 1:4], 3)
+
+
+###################################################
+### code chunk number 17: varclus (eval = FALSE)
+###################################################
+## library(Hmisc)
+## plot(varclus(netw.indic.webs), cex=0.8)
+## abline(h=0.5, lty=2, col="grey")
+
+
+###################################################
+### code chunk number 18: Intro2bipartite.Rnw:533-539 (eval = FALSE)
+###################################################
+## data(Safariland)
+## Iobs <- nestednodf(Safariland)$statistic[3]
+## nulls <- nullmodel(web=Safariland, N=1000, method='r2d') # takes a while!
+## Inulls <- sapply(nulls, function(x) nestednodf(x)$statistic[3])
+## plot(density(Inulls), xlim=c(0, 100), lwd=2, main="NODF")
+## abline(v=Iobs, col="red", lwd=2)
+
+
+###################################################
+### code chunk number 19: Intro2bipartite.Rnw:571-580
+###################################################
+weblist <- lapply(c("Safariland", "vazarr", "vazllao", "vazcer", "vazmasc", 
+                       "vazmasnc", "vazquec", "vazquenc"), get)
+# Write a function to compute the desired statistic, e.g. the difference 
+# between grazed and ungrazed:
+meandiff <- function(webs){
+   obs <- sapply(webs, networklevel, index="linkage density")  
+   mean(obs[1:4] - obs[5:8])
+}
+(observed <- meandiff(weblist))
+
+
+###################################################
+### code chunk number 20: Intro2bipartite.Rnw:583-585
+###################################################
+nulllist <- lapply(weblist, nullmodel, N=1, method="r2d")
+meandiff(weblist)
+
+
+###################################################
+### code chunk number 21: Intro2bipartite.Rnw:588-593
+###################################################
+res <- 1:5000
+for (i in 1:5000){ # takes a few minutes !!
+   nulllist <- sapply(weblist, nullmodel, N=1, method="r2d")
+   res[i] <- meandiff(nulllist)  
+}
+
+
+###################################################
+### code chunk number 22: Intro2bipartite.Rnw:604-608 (eval = FALSE)
+###################################################
+## hist(res, xlim=c(-0.3, 0.3), border="white", col="grey")
+## abline(v=observed, col="red", lwd=2)
+## # compute p-value as proportion smaller or than observed
+## sum(res < observed)/length(res) * 2 # *2 for two-tailed test
+
+
+###################################################
+### code chunk number 23: Intro2bipartite.Rnw:700-702 (eval = FALSE)
+###################################################
+## library(devtools)
+## install_github(rep="pedroj/bipartite_plots")
+
+
